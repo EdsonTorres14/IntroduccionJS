8efe3694d4881ca22506bd7351066be0e9b5cb5b
Edson <edsontorresrios@gmail.com>
2024-01-05 20:07:35 -0600

    10. Tipos de Dato en js

---
+++ js/03.js
@@ -0,0 +1,61 @@
+// Tipos de datos
+
+// undefined
+// let cliente
+// console.log(typeof cliente)
+
+// Boolean
+// const descuento = true
+//console.log(typeof descuento)
+
+// Number
+// const numero1 = 28.30;
+// const numero2 = 0;
+// const numero3 = 200;
+
+// console.log(typeof numero1);
+// console.log(typeof numero2);
+// console.log(typeof numero3);
+
+
+// String 
+// const alumno = 'Juan';
+// let producto = 'Monito 29 pulgadas';
+
+// const numero = 30;
+// const numero2 = '50';
+// console.log(typeof alumno);
+// console.log(typeof numero);
+// console.log(typeof numero2);
+
+// const numeroGrande = BigInt(1);
+// console.log(typeof numeroGrande);
+
+// No se puede mezclar un bigInt con otro tipo de dato
+
+// Solamente realizando un parseo a tipo Number se puede ocupar la variable numeroGrande
+// const numero = 30;
+// console.log(numero + Number(numeroGrande));
+
+// Symbols
+// const primerSymbol = Symbol(1);
+// const segundoSymbol = Symbol(1);
+
+// const numero = 10;
+// const numero2 = 10;
+
+//Un Symbol siempre son diferentes
+// console.log(primerSymbol == segundoSymbol);
+
+//Object
+// const numeros = [1, 2, 3]
+// const descuento = null
+// const alumno = { };
+// console.log(typeof numeros);
+// console.log(typeof descuento);
+// console.log(typeof alumno);
+
+
+
+
+
