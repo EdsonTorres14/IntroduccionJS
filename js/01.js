bfa3109262036eff1ef1ccdf23c563c450d3309b
Edson <edsontorresrios@gmail.com>
2024-01-05 19:31:32 -0600

    first commit

---
+++ js/01.js
@@ -0,0 +1,17 @@
+//Variables con let
+
+let cliente = "Juan";
+
+//FormaCamelcase
+let precioDescuento = 200;
+
+//Se puede reasignar
+cliente = 20;
+cliente = true;
+
+let precio;
+
+precio = 300;
+
+console.log(cliente);
+console.log(precio);
