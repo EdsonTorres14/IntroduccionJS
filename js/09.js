c2f8ebdd1ae9d5c606fbed7258a362aca1af7ce0
Edson <edsontorresrios@gmail.com>
2024-01-19 19:33:51 -0600

    16. Introducción a Arrays

---
+++ js/09.js
@@ -0,0 +1,9 @@
+// Array o Arreglos 
+
+const tecnologias = ['Vue.js', 'JavaScript', 'Node.js', 30, 20, true, {}]
+
+// console.log(tecnologias)
+// console.table(tecnologias)
+
+
+console.log(tecnologias);
