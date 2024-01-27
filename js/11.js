5be2f276c8d9b18b41ad314986acbbeb9db2a798
Edson <edsontorresrios@gmail.com>
2024-01-26 19:08:22 -0600

    18. Destructuring de arrays

---
+++ js/11.js
@@ -0,0 +1,17 @@
+// Destructuring de arrays
+
+const tecnologias = ['Vue.js', 'JavaScript', 'Node.js', 'HTML', 'CSS']
+
+//Destructuring - version corta
+const [vuejs, js, , html] = tecnologias
+
+
+//Destructuring - version larga
+// const vuejs = tecnologias[0]
+// const js = tecnologias[1]
+
+
+console.log(vuejs)
+console.log(js)
+console.log(html)
+
