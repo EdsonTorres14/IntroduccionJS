7b2f78ea6ada85dabf97aa3fb89bbd142a28f22d
Edson <edsontorresrios@gmail.com>
2024-02-02 18:36:38 -0600

    27. Operador Ternario

---
+++ js/20.js
@@ -0,0 +1,9 @@
+// Operador ternario  -> condición ? si se cuple : si no se cumple
+
+const disponible = 100
+const total = 500
+const tarjeta = true
+
+disponible > total || tarjeta ? 
+    console.log('Si puedes pagar') : 
+    console.log('No puedes pagar')
