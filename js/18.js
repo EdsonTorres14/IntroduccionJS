23ea2d7a90b9c06327848816b0ea89640bd584d5
Edson <edsontorresrios@gmail.com>
2024-02-02 18:18:50 -0600

    25. Operadores de Comparación == y === y porque evaluan diferente las variables

---
+++ js/18.js
@@ -0,0 +1,17 @@
+// Operador de comparación
+
+const numero1 = 20
+const numero2 = 200
+
+// === Verifica igualdad en tipo de dato y valor
+// == Verifica igualdad en valor
+
+
+if (numero1 === numero2) {
+    console.log('Si son iguales')
+} else {
+    console.log('No, no son iguales')
+}
+
+console.log(typeof numero1);
+console.log(typeof numero2);
