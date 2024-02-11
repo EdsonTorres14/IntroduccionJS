745f6b9fdf0326693b01a24bc42b5bc4d38ce865
Edson <edsontorresrios@gmail.com>
2024-02-11 10:13:45 -0600

    30. Seleccionando Elementos del DOM con queryselector

---
+++ js/23.js
@@ -0,0 +1,16 @@
+// Limita a un resultado (querySelector)
+const heading = document.querySelector('.heading')
+
+console.log(heading)
+console.log(heading.textContent)
+console.log(heading.innerHTML)
+console.log(heading.tagName)
+console.log(heading.classList)
+console.log(heading.id)
+
+// heading.textContent = 'Nuevo Heading'
+
+
+// Trae todos los elementos (querySelectorAll)
+const enlaces = document.querySelectorAll('.navegacion a')
+console.log(enlaces)
